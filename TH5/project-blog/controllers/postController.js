f2a58063c583a2bb762fce8320002214dbdf211f
kaka601 <vinhhahaok12@gmail.com>
2026-04-03 18:17:26 +0700

    fix(TH5): xử lý lỗi CastError gây crash server khi truy vấn với params không hợp lệ

--- TH5/project-blog/controllers/postController.js
+++ TH5/project-blog/controllers/postController.js
@@ -1,8 +1,12 @@
 const BlogPost = require('../models/BlogPost');
 
 exports.getAllPosts = async (req, res) => {
-  const posts = await BlogPost.find().sort({ createdAt: -1 });
-  res.render('index', { posts });
+  try {
+    const posts = await BlogPost.find().sort({ createdAt: -1 });
+    res.render('index', { posts });
+  } catch (error) {
+    res.status(500).send('Lỗi máy chủ');
+  }
 };
 
 exports.getNewPostForm = (req, res) => {
@@ -10,30 +14,54 @@ exports.getNewPostForm = (req, res) => {
 };
 
 exports.createPost = async (req, res) => {
-  const { title, content, author } = req.body;
-  await new BlogPost({ title, content, author }).save();
-  res.redirect('/');
+  try {
+    const { title, content, author } = req.body;
+    await new BlogPost({ title, content, author }).save();
+    res.redirect('/');
+  } catch (error) {
+    res.status(500).send('Lỗi khi tạo bài viết');
+  }
 };
 
 exports.getPostById = async (req, res) => {
-  const post = await BlogPost.findById(req.params.id);
-  if (!post) return res.status(404).send('Bài viết không tồn tại');
-  res.render('detail', { post });
+  try {
+    const post = await BlogPost.findById(req.params.id);
+    if (!post) return res.status(404).send('Bài viết không tồn tại');
+    res.render('detail', { post });
+  } catch (error) {
+    if (error.name === 'CastError') return res.status(404).send('Bài viết không tồn tại');
+    res.status(500).send('Lỗi máy chủ');
+  }
 };
 
 exports.getEditPostForm = async (req, res) => {
-  const post = await BlogPost.findById(req.params.id);
-  if (!post) return res.status(404).send('Bài viết không tồn tại');
-  res.render('edit', { post });
+  try {
+    const post = await BlogPost.findById(req.params.id);
+    if (!post) return res.status(404).send('Bài viết không tồn tại');
+    res.render('edit', { post });
+  } catch (error) {
+    if (error.name === 'CastError') return res.status(404).send('Bài viết không tồn tại');
+    res.status(500).send('Lỗi máy chủ');
+  }
 };
 
 exports.updatePost = async (req, res) => {
-  const { title, content, author } = req.body;
-  await BlogPost.findByIdAndUpdate(req.params.id, { title, content, author });
-  res.redirect('/');
+  try {
+    const { title, content, author } = req.body;
+    await BlogPost.findByIdAndUpdate(req.params.id, { title, content, author });
+    res.redirect('/');
+  } catch (error) {
+    if (error.name === 'CastError') return res.status(404).send('Bài viết không tồn tại');
+    res.status(500).send('Lỗi máy chủ');
+  }
 };
 
 exports.deletePost = async (req, res) => {
-  await BlogPost.findByIdAndDelete(req.params.id);
-  res.redirect('/');
+  try {
+    await BlogPost.findByIdAndDelete(req.params.id);
+    res.redirect('/');
+  } catch (error) {
+    if (error.name === 'CastError') return res.status(404).send('Bài viết không tồn tại');
+    res.status(500).send('Lỗi máy chủ');
+  }
 };
